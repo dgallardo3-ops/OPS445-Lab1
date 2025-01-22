254e20cfb16da65261b7a70b0ae0cd098675735e
Denzel Gallardo <denzel.gallardo04@gmail.com>
2025-01-22 16:03:52 -0500

    added lab1c.py

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Define the string variable
+name = "Isaac"
+
+# Define the integer variable
+age = 72
+
+# Print the output message
+print(name + " is " + str(age) + " years old!")
